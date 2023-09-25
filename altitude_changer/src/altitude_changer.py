51400a3ab1211852f9ebafad424fab8ba003f776
Aarthi <aarthi@goodmachine.studio>
2023-09-25 12:53:59 -0700

    Change script to change longitutde for a 3 hour test

--- altitude_changer/src/altitude_changer.py
+++ altitude_changer/src/altitude_changer.py
@@ -5,9 +5,17 @@ import requests
 """
 A small script to increase the altitude and spit out a CSV that we can feed into gps-sdr-sim
 """
-# Reykjavík, Iceland :) 
-LAT = 64.128288
-LON = -21.827774
+
+LENGTH_OF_TEST = 3 # in hours
+HZ = 10 
+# TOTAL SAMPLES TO RUN FOR 3 HOURS
+NUM_SAMPLES = 3600 * HZ * LENGTH_OF_TEST
+# LAUNCH SITE 
+LAT = 25.5
+LON = -145.1
+# HORIZONTAL SPEED
+SPEED = 4 # M/S
+KM_PER_DEGREE = 111.32 # 1 decimal degree/111.32 km
 
 ALT_OF_INTEREST = 20000 # meters. 20 km is when most IMU chips will cut out
 MAX_ALT = 30000 # meters
@@ -16,6 +24,8 @@ STARTING_ALT = 0 # meters
 COUNTER_DELT = 0.1
 
 
+def meters_to_decimal_degrees(meter, latitude):
+    return meter/((KM_PER_DEGREE*1000*Math.Cos(latitude*(Math.PI/180))))
 
 def up(alt, meters):
     return alt + meters
@@ -28,16 +38,17 @@ def main():
     longs = list()
     alt = STARTING_ALT
     lon = LON
-    while (alt < MAX_ALT):
-        alt = up(alt, ALT_DELTA)
-        alts.append(alt)
-        lon += .0001
-        longs.append(lon)
-    while (alt > STARTING_ALT):
-        alt = down(alt, ALT_DELTA)
-        alts.append(alt)
-        lon -= .0001
-        longs.append(lon)
+    while(len(alts)<NUM_SAMPLES):
+        while (alt < MAX_ALT):
+            alt = up(alt, ALT_DELTA)
+            alts.append(alt)
+            lon += (meters_to_decimal_degrees(SPEED,LAT))
+            longs.append(lon)
+        while (alt > STARTING_ALT):
+            alt = down(alt, ALT_DELTA)
+            alts.append(alt)
+            lon -= (meters_to_decimal_degrees(SPEED,LAT))
+            longs.append(lon)
     counters = np.arange(0, 0 + len(alts) * 0.1, 0.1).tolist()
     counters = np.round(counters,1)
     with open("out.csv", "w", newline="") as f:
